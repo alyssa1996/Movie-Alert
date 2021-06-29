db4c049bede842fb4192705ec7a433ee72c50de7
지수 박 <alyssapark96@gmail.com>
2021-06-29 10:57:41 +0900

    get movie title using BeautifulSoup select() method

--- crawling_first.py
+++ crawling_first.py
@@ -17,4 +17,9 @@ from bs4 import BeautifulSoup
 
 url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20210701"
 html = requests.get(url)
-print(html.text)
+#print(html.text)
+
+soup = BeautifulSoup(html.text, 'html.parser')
+movie_list = soup.select('div.info-movie')
+for i in movie_list:
+    print(i.select_one('a > strong').text.strip())
